5829629b3b0e81e166899405f091f5295e11a71d
entbappy <entbappy73@gmail.com>
2023-12-20 16:02:10 +0600

    folder structure added

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "research/trials.ipynb",
+    "app.py",
+    "store_index.py",
+    "static/.gitkeep",
+    "templates/chat.html"
+
+]
+
+
+for filepath in list_of_files:
+   filepath = Path(filepath)
+   filedir, filename = os.path.split(filepath)
+
+   if filedir !="":
+      os.makedirs(filedir, exist_ok=True)
+      logging.info(f"Creating directory; {filedir} for the file {filename}")
+
+   if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+      with open(filepath, 'w') as f:
+         pass
+         logging.info(f"Creating empty file: {filepath}")
+
+   else:
+      logging.info(f"{filename} is already created")
+      
+      
+    
