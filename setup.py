da4eaa9bbb1d811e12f63f1a5c3d9cbae70415f5
entbappy <entbappy73@gmail.com>
2023-12-20 16:10:44 +0600

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Boktiar Ahmed Bappy',
+    author_email= 'entbappy73@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
