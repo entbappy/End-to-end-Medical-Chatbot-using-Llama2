47b1f324d3e92a0cf08bf20f388fd3deea5ee905
entbappy <entbappy73@gmail.com>
2023-12-20 17:09:56 +0600

    completed

--- app.py
+++ app.py
@@ -54,6 +54,17 @@ def index():
 
 
 
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port= 8080, debug= True)
 
