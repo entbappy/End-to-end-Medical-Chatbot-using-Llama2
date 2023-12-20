83b768e0db256a4add47cc1f16cbcf1948023c77
entbappy <entbappy73@gmail.com>
2023-12-20 16:40:21 +0600

    store index added

---
+++ src/helper.py
@@ -0,0 +1,30 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+#Extract data from the PDF
+def load_pdf(data):
+    loader = DirectoryLoader(data,
+                    glob="*.pdf",
+                    loader_cls=PyPDFLoader)
+    
+    documents = loader.load()
+
+    return documents
+
+
+
+#Create text chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+
+    return text_chunks
+
+
+
+#download embedding model
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
---
+++ store_index.py
@@ -0,0 +1,28 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+# print(PINECONE_API_KEY)
+# print(PINECONE_API_ENV)
+
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+#Initializing the Pinecone
+pinecone.init(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV)
+
+
+index_name="medical-bot"
+
+#Creating Embeddings for Each of The Text Chunks & storing
+docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
